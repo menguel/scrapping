aea97499e86cead56dcda22edbdc3c615a0edf85
melono <melono@irex.aretex.ca>
2024-07-29 22:54:41 +0100

    Desable warning error

--- brightdata_script.py
+++ brightdata_script.py
@@ -2,11 +2,16 @@ import sys
 import json
 
 import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://lumtest.com/myip.json"
 
 proxies = {'http': 'http://brd-customer-hl_20768c74-zone-zone1:3n7z29me5mr6@brd.superproxy.io:22225',
             'https': 'http://brd-customer-hl_20768c74-zone-zone1:3n7z29me5mr6@brd.superproxy.io:22225'}
+
+cert_path = "/home/joe/Téléchargements/ca.crt"
 
 
 response = requests.get(url, proxies=proxies, verify=False)
